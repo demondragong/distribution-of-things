4dabc0785461f9d6fc860cc4077acb646860a46c
Natascha Minnitt <nataschaminnitt@Nataschas-MacBook-Air-2.local>
2021-12-10 17:17:52 +0200

    First version of app to github

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+def post_detail(request, slug=None):
+    if slug is None:
+        try:
+            post = Post.objects.filter(created_on__isnull=False).latest('created_on')
+        except Post.DoesNotExist:
+            raise Http404("Post does not exist")
+    else:
+        post = get_object_or_404(Post, slug=slug)
+
+    try:
+        next_post = post.get_next_by_created_on()
+    except Post.DoesNotExist:
+        next_post = None
+
+    try:
+        previous_post = post.get_previous_by_created_on()
+    except Post.DoesNotExist:
+        previous_post = None
+
+    return render(request, 'blog/index.html', {
+        'post': post,
+        'next_post': next_post,
+        'previous_post': previous_post
+    })
+
+
+def about(request):
+    return render(request, 'blog/about.html')
