d7020939fad678c5a77174beb445724813f83586
Natascha Minnitt <nataschaminnitt@Nataschas-Air-2.lan>
2021-12-17 19:00:15 +0200

    oups

--- dot_project/settings.py
+++ dot_project/settings.py
@@ -9,15 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-486zo5pev)vpw_5d15*z(cd+(lkauef5z)p6jxd#gh_mwvmwv@'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-486zo5pev)vpw_5d15*z(cd+(lkauef5z)p6jxd#gh_mwvmwv@')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG =
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.52','127.0.0.1','0.0.0.0','192.168.0.111','192.168.106.199']
+ALLOWED_HOSTS = ['distributionofthings.herokuapp.com','.distributionofthings.com','127.0.0.1']
 
 
 # Application definition
@@ -34,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,8 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -120,3 +117,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
